60126c013cc38bbc710d054034d9d036bea34604
pedro28rei <a26013@alunos.ipca.pt>
2024-04-04 21:28:56 +0100

    Commite test

--- API/models/user.js
+++ API/models/user.js
@@ -5,7 +5,7 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
-     * Helper method for defining associations.
+     * Helper method for defining associations. GG gay
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
