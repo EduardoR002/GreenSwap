6e8f4738cf2fcad06082753d5e03323836d0aa09
Eduardo Rebelo <100939320+EduardoR002@users.noreply.github.com>
2024-05-06 17:09:06 +0100

    Update user.controller.js

--- API/controllers/user.controller.js
+++ API/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const models = require('../models')
 
+// Function that create a new User
 function createUser(req, res){
     const user = {
         name: req.body.name,
@@ -10,49 +11,56 @@ function createUser(req, res){
         description: req.body.description,
         photo: req.body.photo
     }
+    
+    if (user.phone.length !== 9 || isNaN(user.phone)) {
+        return res.status(422).json({
+            message: "Phone number must be 9 digits long"
+        });
+    }
 
-        // verify if email and phone are unique
-        models.user.findOne({ 
-            where: {
-                [models.Sequelize.Op.or]: [
-                    { email: req.body.email },
-                    { phone: req.body.phone }
-                ]
-            }
-        }).then(existingUser => {
-            if (existingUser) {
-                // If the email already exists, returns error 409
-                if (existingUser.email === req.body.email) {
-                    return res.status(409).json({
-                        message: "Email already exists"
-                    });
-                }
-                // If the phone already exists, returns error 409
-                else if (existingUser.phone === req.body.phone) {
-                    return res.status(409).json({
-                        message: "Phone number already exists"
-                    });
-                }
-            }else {
-                // Caso o email e o nu, criar o novo usuário
-                models.user.create(user).then(result => {
-                    res.status(200).json({
-                        message: "User created successfully",
-                        user: result
-                    });
-                }).catch(error => {
-                    res.status(500).json({
-                        message: "Something went wrong",
-                        error: error
-                    });
+
+    // verify if email and phone are unique
+    models.user.findOne({ 
+        where: {
+            [models.Sequelize.Op.or]: [
+                { email: req.body.email },
+                { phone: req.body.phone }
+            ]
+        }
+    }).then(existingUser => {
+        if (existingUser) {
+            // If the email already exists, returns error 409
+            if (existingUser.email === req.body.email) {
+                return res.status(409).json({
+                    message: "Email already exists"
                 });
             }
-        }).catch(error => {
-            res.status(500).json({
-                message: "Something went wrong",
-                error: error
+            // If the phone already exists, returns error 409
+            else if (existingUser.phone === req.body.phone) {
+                return res.status(409).json({
+                    message: "Phone number already exists"
+                });
+            }
+        }else {
+            // If the email and the phone are new in the database, then proceed to create a new user
+            models.user.create(user).then(result => {
+                res.status(200).json({
+                    message: "User created successfully",
+                    user: result
+                });
+            }).catch(error => {
+                res.status(500).json({
+                    message: "Something went wrong",
+                    error: error
+                });
             });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong",
+            error: error
         });
+    });
 }
 
 module.exports = {
