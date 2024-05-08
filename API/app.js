ff2e5a717a0e72dea443c32956fd4b25084852a5
Rui Rodrigues <120216803+RuiRodrigues17@users.noreply.github.com>
2024-05-08 16:03:25 +0100

    Alteração app

--- API/app.js
+++ API/app.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const tnotificRoute = require('./routes/tnotific.js');
+const usersRoute = require('./routes/users.js');
 const tproductRoute = require('./routes/tproduct.js');
-const tnotificationRoute = require('./routes/tnotification.js');
 
 
 app.use(bodyParser.json());
@@ -22,6 +22,5 @@ app.use("/tnotific", tnotificRoute);
 app.use("/tproduct", tproductRoute);
 app.use("/users", usersRoute);
 app.use("/tproduct", tproductRoute);
-app.use("/tnotification", tnotificationRoute);
 
 module.exports = app;
