6097ddb735286efa5ba9a16fd237ab9fd50298c7
Rui Rodrigues <120216803+RuiRodrigues17@users.noreply.github.com>
2024-05-08 15:56:44 +0100

    Alteração

--- API/routes/tnotification.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { createTypeNotification } = require('../controllers/tnotification.controller.js')
-
-const router = Router();
-
-router.post("/create", createTypeNotification);
-
-
-module.exports = router;
