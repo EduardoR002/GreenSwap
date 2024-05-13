04e98c4e95e550523f1d40e6f4e578547178c230
pedro28rei <a26013@alunos.ipca.pt>
2024-05-13 03:02:46 +0100

    Website working on REACT

--- website/src/Components/logIn.js
+++ website/src/Components/logIn.js
@@ -1,7 +1,49 @@
-import '../CS/logIn.css';
+import '../CSS/logIn.css';
 
-//Function that will apresent Login page of the website
-function logInPage(){
+function Login() {
+    // Função para lidar com o clique no botão de login
+    const login = () => {
+        // Coloque aqui a lógica de login
+        console.log('Login clicked');
+    };
 
-    <p>Olá</p>
+    return (
+        <div>
+            <div id="bg"></div>
+
+            <div className="main">
+                {/* Ícone clicável que leva o usuário para a página inicial */}
+                <div className="iconDiv">
+                    <a href="home.html">
+                        <img src="../images/GreenSwap.png" style={{ width: '60%', height: '60%' }} className="icon" alt="GreenSwap Icon" />
+                    </a>
+                </div>
+
+                {/* Formulário */}
+                <div className="formDiv">
+                    <span className="poppins-regular greetings"><b>Welcome!</b></span>
+
+                    <div className="input-box">
+                        <span className="material-symbols-outlined">
+                            person
+                        </span>
+                        <input className="poppins-regular textBox" type="text" id="email" placeholder="Email" />
+                    </div>
+
+                    <div className="input-box">
+                        <span className="material-symbols-outlined">
+                            lock
+                        </span>
+                        <input className="poppins-regular textBox" type="password" id="password" placeholder="Password" />
+                    </div>
+                    
+                    <button className="poppins-regular" onClick={login}>Login</button>
+
+                    <span className="poppins-regular linknote">Not registered? <a href="register.html">Create an account</a></span>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default Login;
