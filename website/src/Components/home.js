c4621632a690a4f7bb1279bd1bda919d1339927d
ajoa00liveira <a26001@alunos.ipca.pt>
2024-05-14 12:30:33 +0100

    Stashed (small changes)
    ---

--- website/src/Components/home.js
+++ website/src/Components/home.js
@@ -4,7 +4,6 @@ import '../CSS/navbar.css';
 //import Navbar from './Components/navBar';
 
 //Function that will apresent website Home page
-<<<<<<< Updated upstream
 function Home() {
 
     return(
@@ -13,7 +12,6 @@ function Home() {
             <button type="submit">Search</button>
         </div>
     )
-
 
     //Arranjar forma de meter a navbar, provavelmente com routes, pra ir buscar o componenten Navbar
     /*
@@ -26,23 +24,3 @@ function Home() {
 }
 
 export default Home;
-=======
-export default function Home() {
-    return(
-        <>
-            {/*Navigation Topper*/}
-            <div id="navigation" />
-            {/*Page Content*/}
-            {/*Text box that allows the user to write the product they want to search for*/}
-            <div className="search-container">
-                <input type="text" placeholder="Search..." />
-                <button type="submit">Search</button>
-            </div>
-            <a href="logIn.html">
-                Login
-            </a>
-        </>
-    );
-}
-
->>>>>>> Stashed changes
