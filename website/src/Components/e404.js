84f23823fa53f2b03326d41a23f8c69cade01d77
ajoa00liveira <a26001@alunos.ipca.pt>
2024-05-15 17:14:32 +0100

    e404 page style fix

--- website/src/Components/e404.js
+++ website/src/Components/e404.js
@@ -6,10 +6,13 @@ export default function E404(){
     return(
         <>
         <div className='container-02'>
-            <div className='part-02' />
+            <div className='part1-02'>
+                <h1 className='poppins-regular h1-02'>404</h1>
+                <h2 className='poppins-regular h2-02'>Page not found, please go back</h2>
+            </div>
             <div className='gif-02' />
-            <div className='part-02' />
-            <div className='part-02'>
+            <div className='part2-02' />
+            <div className='part3-02'>
                 <button className='button-04 button-02'>
                     <Link className='poppins-regular link-04' to="/">Home</Link>
                 </button>
