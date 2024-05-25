e27e8ad437d027b60ad2ba4acf9c238e14587385
ajoa00liveira <a26001@alunos.ipca.pt>
2024-05-26 00:11:20 +0100

    update

--- website/src/Components/productPage.js
+++ website/src/Components/productPage.js
@@ -6,6 +6,8 @@ import Navbar from './navbar';
 function ProductPage() {
   const { productId } = useParams();
   const [productData, setProductData] = useState(null);
+  const [userData, setUserData] = useState(null);
+  var sellerId;
 
   useEffect(() => {
     async function fetchProduct() {
@@ -22,6 +24,7 @@ function ProductPage() {
 
         if (res.ok) {
           setProductData(data.product); // Armazenar os dados do produto em productData
+          sellerId = productData.idseller;
         } else {
           console.error('Failed to fetch product:', res.status, res.statusText);
         }
@@ -30,30 +33,71 @@ function ProductPage() {
       }
     }
 
-    fetchProduct(); // Chamando a função para buscar o produto ao montar o componente
-  }, [productId]);
+    async function fetchSeller() {
+        try {
+          const selPath = 'http://localhost:3000/users/getuser/' + 32;
+          const res = await fetch(selPath, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          });
+  
+          const data = await res.json();
+  
+          if (res.ok) {
+            setUserData(data.user); // Armazenar os dados do produto em userData
+          } else {
+            console.error('Failed to fetch seller:', res.status, res.statusText);
+          }
+        } catch (error) {
+          console.error('Error:', error.message);
+        }
+      }
 
+    fetchProduct(); // Chamando a função para buscar o produto ao montar o componente
+    fetchSeller();
+
+  }, [productId]);
+  
   return (
     <>
         <div className="navbar-position"> {/* Navbar common to all pages*/}
             <Navbar />
-            {productData ? (
+            {productData && userData ? (
                 <>
-                    <div className='prod-cont'>
-                        <div className='prod-cont-left'>
-                            <div className='prod-frame' />
+                    <div className='prod-cont-13'>
+                        <div className='prod-cont-left-13'>
+                            <div className='prod-frame-13' />
                         </div>
-                        <div className='prod-cont-right'>
-                            <span className='poppins-regular'>{productData.name}</span>
+                        <div className='prod-cont-right-13'>
+                            <div className='pct-up'>
+                                <span className='poppins-regular prod-name-13'>{productData.name}</span>
+                                <span className='poppins-regular user-name-13'>{userData.name}</span>
+                                <span className='poppins-regular prod-price-13'>{productData.price}€/Kg</span>
+                                <span className='poppins-regular prod-desc-13'>{productData.description}</span>
+                            </div>
+
+                            <div className='pct-down'>
+                                <div className=''>
+                                    <input className='poppins-regular input-13' placeholder='Novo Preço'></input>
+                                </div>
+                                <div>
+                                    <input className='poppins-regular input-13' placeholder='Quantidade'></input>
+                                </div>
+                                
+                                
+                            </div>
+                            
                         </div>
                     </div>
 
-                    <div className='more-prods-cont'>
+                    <div className='more-prods-cont-13'>
 
                     </div>
                 </>
             ) : (
-                <p>Loading...</p>
+                <p className='poppins-regular'>Loading...</p>
             )}
             
         </div>
