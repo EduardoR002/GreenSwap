00a5b39ed8ed940b6e69bc20580c126fa19daf12
pedro28rei <a26013@alunos.ipca.pt>
2024-05-27 17:06:28 +0100

    User Purchases Page (UPDATE)

--- website/src/Components/orders.js
+++ website/src/Components/orders.js
@@ -13,11 +13,43 @@ function Orders() {
     <div className="navbar-position"> {/* Navbar common to all pages*/}
     <Navbar />
 
-    {/* Page title */}
-    <h1 className="title-07">Orders</h1>
+      <div className="orders-container-10">
 
+          <div className='product-container-10'>
 
+            <div className='left-content'>
+
+              <Link to={'../products/'}  className="Link">
+                                <img className='product-img-03' src='https://picsum.photos/328/204'/>
+              </Link>
+                            <span className='poppins-regular product-h1-03'>{}Tomates</span>
+                            <span className='poppins-regular product-h2-03'>{}15€/kg</span>
+                            <div className='seller-03'>
+                                <span class="material-symbols-outlined mso-03">
+                                    person
+                                </span>
+                                <span className='poppins-regular seller-h3-03'>{}John</span>
+                            </div>
+            </div>
+                            <div>
+            <div className='right-content'>
+              <span className="poppins-regular product-info-h1-03"><b>Purchase Type:</b> {} </span><br></br>
+              <span className="poppins-regular product-info-h1-03"><b>Purchase Date:</b> {} </span><br></br>
+              <span className="poppins-regular product-info-h1-03"><b>Quantity:</b> {} kg </span><br></br>
+              <span className="poppins-regular product-info-h1-03"><b>Price:</b> $ </span><br></br>
+              <span className="poppins-regular product-info-h1-03"><b>Date Receveid (estimated):</b> {} </span><br></br>
+
+            </div>
+
+          </div>
+
+          </div>
+         
+      
       </div>
+
+
+    </div>
     );
   }
 
