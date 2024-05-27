dff5e0a98f078dafec8413016ea4805568e0099f
pedro28rei <a26013@alunos.ipca.pt>
2024-05-27 20:32:17 +0100

    Proposal Page (UPDATE)

--- website/src/Components/orders.js
+++ website/src/Components/orders.js
@@ -4,46 +4,55 @@ import { Link } from "react-router-dom";
 import '../CSS/navbar.css';
 import Navbar from './navbar';
 
-//Function that will present About Us page of the website
+//Function that will present Orders page of the website
 function Orders() {
+
+  //fazer função pra ir buscar as compras do user, e a sua informação e do produto que comprou e depois meter as variaveis em baixo no HTML ENTRE OS {}
+
 
   return (
    
 
     <div className="navbar-position"> {/* Navbar common to all pages*/}
-    <Navbar />
+      <Navbar />
 
+      {/* Div where will apear user purchases*/}
       <div className="orders-container-10">
 
+          {/* Div for product information and picture*/}
           <div className='product-container-10'>
 
-            <div className='left-content'>
+                <div className='left-content-10'>
 
-              <Link to={'../products/'}  className="Link">
+                  <Link to={'../products/'}  className="Link">
                                 <img className='product-img-03' src='https://picsum.photos/328/204'/>
-              </Link>
-                            <span className='poppins-regular product-h1-03'>{}Tomates</span>
-                            <span className='poppins-regular product-h2-03'>{}15€/kg</span>
-                            <div className='seller-03'>
-                                <span class="material-symbols-outlined mso-03">
+                  </Link>
+                            <span className='poppins-regular product-h1-10'>{}Tomates</span>
+                            <span className='poppins-regular product-h2-10'>{}15€/kg</span>
+                            <div className='seller-10'>
+                            <span class="material-symbols-outlined mso-10">
                                     person
-                                </span>
-                                <span className='poppins-regular seller-h3-03'>{}John</span>
-                            </div>
+                            </span>
+                            <span className='poppins-regular seller-h3-10'>{}John</span>
+                  </div>
             </div>
-                            <div>
-            <div className='right-content'>
-              <span className="poppins-regular product-info-h1-03"><b>Purchase Type:</b> {} </span><br></br>
-              <span className="poppins-regular product-info-h1-03"><b>Purchase Date:</b> {} </span><br></br>
-              <span className="poppins-regular product-info-h1-03"><b>Quantity:</b> {} kg </span><br></br>
-              <span className="poppins-regular product-info-h1-03"><b>Price:</b> $ </span><br></br>
-              <span className="poppins-regular product-info-h1-03"><b>Date Receveid (estimated):</b> {} </span><br></br>
+            <div>
+
+                {/* Div for purchase information*/}
+                <div className='right-content-10'>
+
+                  <span className="poppins-regular product-info-h1-10"><b>Purchase Type:</b> {} </span><br></br>
+                  <span className="poppins-regular product-info-h1-10"><b>Purchase Date:</b> {} </span><br></br>
+                  <span className="poppins-regular product-info-h1-10"><b>Quantity:</b> {} kg </span><br></br>
+                  <span className="poppins-regular product-info-h1-10"><b>Price:</b> $ </span><br></br>
+                  <span className="poppins-regular product-info-h1-10"><b>Date Receveid (estimated):</b> {} </span><br></br>
+                  <span className="poppins-regular product-info-h1-10"><b>State: </b> {} </span><br></br>
 
             </div>
 
           </div>
 
-          </div>
+      </div>
          
       
       </div>
--- website/src/Components/proposals.js
+++ website/src/Components/proposals.js
@@ -1,24 +1,60 @@
 import React, { useRef }  from "react";
 import '../CSS/ranking.css' //TO CHANGE
 import { Link } from "react-router-dom";
+import "../CSS/proposals.css"
 import '../CSS/navbar.css';
 import Navbar from './navbar';
 
-//Function that will present About Us page of the website
+//Function that will present proposal page of the website
 function Proposals() {
 
     return (
-     
 
       <div className="navbar-position"> {/* Navbar common to all pages*/}
       <Navbar />
 
-      {/* Page title */}
-      <h1 className="title-07">Your Proposals</h1>
+      {/* Div where will apear user proposals*/}
+      <div className="proposal-container-14">
+
+          {/* Div for product information and picture*/}
+          <div className='product-container-14'>
+
+                <div className='left-content-14'>
+
+                  <Link to={'../products/'}  className="Link">
+                                <img className='product-img-03' src='https://picsum.photos/328/204'/>
+                  </Link>
+                            <span className='poppins-regular product-h1-14'>{}Tomates</span>
+                            <span className='poppins-regular product-h2-14'>{}15€/kg</span>
+                            <div className='seller-03'>
+                            <span class="material-symbols-outlined mso-14">
+                                    person
+                            </span>
+                            <span className='poppins-regular seller-h3-14'>{}John</span>
+                  </div>
+            </div>
+            <div>
+
+                {/* Div for proposal information*/}
+                <div className='right-content-14'>
+
+                  <span className="poppins-regular product-info-h1-14"><b>Purchase Type:</b> {} </span><br></br>
+                  <span className="poppins-regular product-info-h1-14"><b>Proposal Date:</b> {} </span><br></br>
+                  <span className="poppins-regular product-info-h1-14"><b>Quantity:</b> {} kg </span><br></br>
+                  <span className="poppins-regular product-info-h1-14"><b>Price:</b> $ </span><br></br>
+                  <span className="poppins-regular product-info-h1-14"><b>State: </b> {} </span><br></br>
+
+            </div>
+
+          </div>
+
+      </div>
+         
+      
+      </div>
 
 
-
-        </div>
+    </div>
       );
     }
 
